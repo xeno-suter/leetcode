19198dfbe8333e5b63f5b5586d2460dce6d3d39d
Xeno Suter <64072009+xeno-suter@users.noreply.github.com>
2024-11-04 10:17:57 +0100

    feat(238): product of array except self
    used prefix and suffix algo

---
+++ 238.product-of-array-except-self.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=238 lang=cpp
+ *
+ * [238] Product of Array Except Self
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1);
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return res;
+    }
+};
+// @lc code=end
