e8cc4bd498540337d86d361da04cb39ef0bea25c
Xeno Suter <64072009+xeno-suter@users.noreply.github.com>
2024-11-05 16:38:44 +0100

    chore(167): two sum ii - optimize space
    Your runtime beats 100 % of cpp submissions
    Your memory usage beats 97.14 % of cpp submissions (18 MB)

--- 167.two-sum-ii-input-array-is-sorted.cpp
+++ 167.two-sum-ii-input-array-is-sorted.cpp
@@ -8,10 +8,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int left = 0, right = numbers.size() - 1;
+        int left = 0, right = numbers.size() - 1, sum = 0;
 
         while (left < right) {
-            int sum = numbers[left] + numbers[right];
+            sum = numbers[left] + numbers[right];
             if (sum == target) {
                 return {left + 1, right + 1};
             } else if (sum < target) {
