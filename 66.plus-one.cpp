f004176d37c6cdf7a63d226c442f145aa2f3aee2
Xeno Suter <64072009+xeno-suter@users.noreply.github.com>
2024-11-09 16:50:43 +0100

    feat(66): plus one
    Your runtime beats 100 % of cpp submissions
    Your memory usage beats 48.41 % of cpp submissions (10.6 MB)

---
+++ 66.plus-one.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=66 lang=cpp
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                ++digits[i];
+                return digits;
+            }
+        }
+
+        digits[0] = 1;
+        digits.push_back(0);
+
+        return digits;
+    }
+};
+// @lc code=end
+
