27851c2e17f68f3db01cb2ab65b266bb4e0c620b
Xeno Suter <64072009+xeno-suter@users.noreply.github.com>
2024-12-10 20:52:02 +0100

    feat(338): counting bits

---
+++ 338.counting-bits.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=338 lang=cpp
+ *
+ * [338] Counting Bits
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n+1, 0);
+
+        for (int i = 1; i <= n; ++i)
+        {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        
+        return res;
+    }
+};
+// @lc code=end
